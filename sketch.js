c3a45e7cfe510ca1492aa0485c55c99daf83c748
kuromi916 <mads189645862@gmail.com>
2025-10-28 19:55:52 -0400

    title change 6

--- sketch.js
+++ sketch.js
@@ -74,7 +74,7 @@ gifFASTEST.delay(playSpeed);
 
 
 image(BG, width/2,height/2,width,height);
-image(Title, width/2,70,400,175);
+image(Title, width/2,70,400,165);
 image(gifFASTEST,width/2,height/2,500,720,);
 
 }
